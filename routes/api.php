02586a89829a206ad4b5eb23a6b38d5694934b6e
Dominic Anuta Vega <davega12.dv@gmail.com>
2025-03-28 16:04:56 +0800

    API Routing

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,10 @@ use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
 Route::apiResource('tasks', TaskController::class);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
