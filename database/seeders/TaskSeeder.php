da89e76417bedee3725a6f144b9feb82c1c3e34a
Dominic Anuta Vega <davega12.dv@gmail.com>
2025-03-28 16:05:11 +0800

    Seeder

--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Task;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,16 @@ class TaskSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Task::create([
+            'id' => 1,
+            'title' => 'First Task',
+            'description' => 'This is the content of the first task.',
+        ]);
+
+        Task::create([
+            'id' => 2,
+            'title' => 'Second Task',
+            'description' => 'This is the content of the second task.',
+        ]);
     }
 }
